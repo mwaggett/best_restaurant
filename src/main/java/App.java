3255e9ac5674bf756165102f48e0517392a951c8
chitra atmakuru & Molly Waggett <student@epicodus.com>
2015-08-26 12:14:48 -0700

    Begin building web interface. Modify layout to include nav bar, create homepage, and add cuisines view.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import java.util.Random;
 import java.util.HashMap;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.List;
 import java.lang.*;
 import static spark.Spark.*;
 import spark.ModelAndView;
@@ -11,26 +12,24 @@ import java.util.Map;
 public class App {
 
   public static void main(String[] args) {
-    // staticFileLocation("/public"); // Relative path for images, css, etc.
-    // String layout = "templates/layout.vtl";
-    //
-    // get("/", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/index.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-    //
-    // get("/newpage", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/newpage.vtl");
-    //
-    //   <Class> param = request.queryParams("param");
-    //   model.put("param", param);
-    //   <Class> result = someMethod(param);
-    //   model.put("result", result);
-    //
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
-  }
+    staticFileLocation("/public"); // Relative path for images, css, etc.
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/cuisines", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/cuisines.vtl");
+
+      List<Cuisine> cuisines = Cuisine.all();
+      model.put("cuisines", cuisines);
+
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+   }
 
 }
